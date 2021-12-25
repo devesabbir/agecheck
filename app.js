609db5c7647deea2060d324c7e540a8b8a425a07
dhrubosabbir <heartedsabbirdhrubo@gmail.com>
2021-12-26 05:02:10 +0600

    cng

--- app.js
+++ app.js
@@ -1,35 +1,25 @@
-const name = document.querySelector('#name')
-const age = document.querySelector('#age')
-const button = document.querySelector('#button')
-const output = document.querySelector('.output')
+const name = document.querySelector("#name");
+const age = document.querySelector("#age");
+const button = document.querySelector("#button");
+const output = document.querySelector(".output");
+const male = document.querySelector("#male");
+const female = document.querySelector("#female");
+const gender = document.querySelector(".gender");
 
-console.log();
-
-
-button.addEventListener('click', () => {
-        
-        if(name.value == ''){
-
-            name.nextElementSibling.innerHTML = `
+button.addEventListener("click", () => {
+  if (name.value == "") {
+    name.nextElementSibling.innerHTML = `
              <p class="text-danger">Name Field Required</p>
-          `
-          
-        }else if(age.value == ''){
-            age.nextElementSibling.innerHTML = `
+          `;
+  } else if (age.value == "") {
+    age.nextElementSibling.innerHTML = `
             <p class="text-danger">Year Field Required</p>
-        ` 
-
-        }else{
-            output.innerHTML = `
-            ${ageCal(name.value,age.value)}
-          `
-         
-       }
- 
-         name.value = '';
-         age.value = '';
-     })
-     
-
-
-
+        `;
+  } else {
+    output.innerHTML = `
+           ${ageCal(name.value, age.value)}
+         `;
+  }
+  name.value = "";
+  age.value = "";
+});
--- function.js
+++ function.js
@@ -1,52 +1,51 @@
-function ageCal(name,year) {
-     let date = new Date();
-     let age = date.getFullYear() - year;
-     return `
-        <p class="bg-${ageCheck(age).class} text-${ageCheck(age).color}">Hi ${name} You Are ${age} Years Old & ${ageCheck(age).status}</p>
+function ageCal(name, year) {
+  let date = new Date();
+  let age = date.getFullYear() - year;
+  return `
+        <p class="bg-${ageCheck(age).class} text-${
+    ageCheck(age).color
+  }">Hi ${name} You Are ${age} Years Old ${ageCheck(age).Status}</p>
      
-     `
+     `;
 }
 
-let ageCheck = (age) => {
+let ageCheck = (age, gender) => {
+  if (age >= 0 && age <= 12) {
+    return {
+        Status: "& tumi Ekhno Babu.",
+        class: "info",
+      };
+    }else if (age >= 13 && age <= 17) {
+        return {
+            Status: "& Tumi Ekhn Teenager.",
+            class: "primary",
+          };
+  } else if (age >= 18 && age <= 30) {
+    return {
+        Status: "& Tumi togboge Young.",
+        class: "primary",
+      };
+  } else if (age >= 31 && age <= 45) {
+    return {
+        Status: "& Apni Ekhn Midle Age.",
+        class: "warning",
+      };
+  } else if (age >= 46 && age <= 120) {
+    return {
+        Status: "& Apni ekhn Oldest Manush.",
+        class: "danger",
+      };
+  } else if (age >= 121) {
+    return {
+      Status: ". R Tumi Buji Jin!",
+      class: "dark",
+      color: "success",
+    };
+  } else {
+    return {
+      Status: ". Asole kothao gormil hoyeche...!",
+      class: "danger"
      
-      if(age >= 0 && age <= 12){
-         return {
-             Status : 'You Are babu',
-             class : 'info'
-         }
-      }else if(age >= 13 && age <= 17 ){
-          return {
-              Status : 'You Are Kishor',
-              class : 'primary'
-          }
-      }else if(age >= 18 && age <= 30){
-          return {
-              status : 'You are Togboge jubok',
-              class : 'success'
-          }
-      }else if(age >= 31 && age <= 45){
-        return {
-            status : 'You are Moddho Boyosko',
-            class : 'warning'
-        }
-      }else if(age >= 46 && age <= 120){
-        return {
-            status : 'You are Buru    Manush',
-            class : 'danger'
-        }
-      }else if(age >= 121 ){
-        return {
-            status : 'Tumi toh Jin',
-            class : 'dark',
-            color : 'success'
-        }
-      }else{
-        return {
-            status : 'kothao gormil ache',
-            class : 'danger'
-        }
-      }
-
-}
- 
-
+    };
+  }
+};
