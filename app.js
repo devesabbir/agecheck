31fe63947e1a739367958a44d5b6e8fde0224af6
dhrubosabbir <heartedsabbirdhrubo@gmail.com>
2021-12-28 00:43:07 +0600

    change a line

--- app.js
+++ app.js
@@ -2,30 +2,24 @@ const name = document.querySelector("#name");
 const age = document.querySelector("#age");
 const button = document.querySelector("#button");
 const output = document.querySelector(".output");
-let gender = document.querySelector('input[type="radio"]:checked');
-
-
 
 button.addEventListener("click", () => {
-
   let gender = document.querySelector('input[type="radio"]:checked');
 
   if (name.value == "") {
     name.nextElementSibling.innerHTML = `
              <p class="text-danger">Name Field Required!</p>
           `;
-  } else if (age.value == "" || (/^[0-9]*$/).test(age.value) == false) {
+  } else if (age.value == "" || /^[0-9]*$/.test(age.value) == false) {
     age.nextElementSibling.innerHTML = `
             <p class="text-danger">Year Field Required! fill with Digit.</p>
         `;
   } else {
     output.innerHTML = `
-             ${ageCal(name.value,age.value,gender.value)}
+             ${ageCal(name.value, age.value, gender.value)}
          `;
   }
-  
+
   name.value = "";
   age.value = "";
 });
-
-
--- function.js
+++ function.js
@@ -1,96 +1,93 @@
-function ageCal(name,year,gender) {
+function ageCal(name, year, gender) {
   let date = new Date();
   let age = date.getFullYear() - year;
   return `
-        <p class="bg-${ageCheck(age,gender).classs} text-${
-    ageCheck(age,gender).color}">Hi ${name} You Are ${age} Years Old ${ageCheck(age,gender).Status}</p>
+        <p class="bg-${ageCheck(age, gender).classs} text-${
+    ageCheck(age, gender).color
+  }">Hi ${name} You Are ${age} Years Old ${ageCheck(age, gender).Status}</p>
      
      `;
 }
 
-let ageCheck = (age,gender) => {
+let ageCheck = (age, gender) => {
   if (age >= 0 && age <= 12) {
-        
-         if(gender == 'male'){
-          return {
-            Status: "& tumi Ekhno Babu.",
-            classs: "info",
-          };
-         }else if(gender == 'female'){
-           return {
-            Status: "& tumi Ekhno Babu.",
-            classs: "info",
-           };   
-         }
-
-
-    }else if (age >= 13 && age <= 17) {
-      if(gender == 'male'){
-        return {
-          Status: "& tumi Ekhn Kishor.",
-          classs: "info",
-        };
-       }else if(gender == 'female'){
-         return {
-          Status: "& tumi Ekhn Kishori.",
-          classs: "info",
-         };   
-       }
+    if (gender == "male") {
+      return {
+        Status: "& tumi Ekhno Babu.",
+        classs: "info",
+      };
+    } else if (gender == "female") {
+      return {
+        Status: "& tumi Ekhno Babu.",
+        classs: "info",
+      };
+    }
+  } else if (age >= 13 && age <= 17) {
+    if (gender == "male") {
+      return {
+        Status: "& tumi Ekhn Kishor.",
+        classs: "info",
+      };
+    } else if (gender == "female") {
+      return {
+        Status: "& tumi Ekhn Kishori.",
+        classs: "info",
+      };
+    }
   } else if (age >= 18 && age <= 30) {
-    if(gender == 'male'){
+    if (gender == "male") {
       return {
         Status: "& tumi Ekhn jubok.",
         classs: "info",
       };
-     }else if(gender == 'female'){
-       return {
+    } else if (gender == "female") {
+      return {
         Status: "& tumi Ekhn jubuti.",
         classs: "info",
-       };   
-     }
+      };
+    }
   } else if (age >= 31 && age <= 45) {
-    if(gender == 'male'){
+    if (gender == "male") {
       return {
         Status: "& Apni Ekhn Purush.",
         classs: "info",
       };
-     }else if(gender == 'female'){
-       return {
+    } else if (gender == "female") {
+      return {
         Status: "& Apni Ekhn Mohila.",
         classs: "info",
-       };   
-     }
+      };
+    }
   } else if (age >= 46 && age <= 120) {
-    if(gender == 'male'){
+    if (gender == "male") {
       return {
         Status: "& Apni Ekhn Buro Manush.",
         classs: "info",
-        color: "danger"
+        color: "danger",
       };
-     }else if(gender == 'female'){
-       return {
+    } else if (gender == "female") {
+      return {
         Status: "& Apni Ekhn Buri Manush.",
         classs: "info",
-        color: "danger"
-       };   
-     }
+        color: "danger",
+      };
+    }
   } else if (age >= 121) {
-    if(gender == 'male'){
+    if (gender == "male") {
       return {
         Status: ". r tumi buji jin !",
         classs: "info",
       };
-     }else if(gender == 'female'){
-       return {
+    } else if (gender == "female") {
+      return {
         Status: ".tumi naki pori !",
         classs: "info",
-       };   
-     }
+      };
+    }
   } else {
     return {
       Status: ". Asole kothao gormil hoyeche...!",
-      classs: "danger"
-     
+      classs: "danger",
     };
   }
 };
