a1c24c3e5fc700e323d5c554a11c113ad0a8d4d6
dhrubosabbir <heartedsabbirdhrubo@gmail.com>
2021-12-28 04:14:45 +0600

    cng

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const age = document.querySelector("#age");
 const button = document.querySelector("#button");
 const output = document.querySelector(".output");
 
+
+
+name.addEventListener('keyup', function(){
+    output.innerHTML = '' ;
+    name.nextElementSibling.innerHTML = '';
+})
+
+age.addEventListener('keyup', function(){
+    output.innerHTML = '' ;
+    age.nextElementSibling.innerHTML = '';
+})
+
+
 button.addEventListener("click", () => {
   let gender = document.querySelector('input[type="radio"]:checked');
 
@@ -18,8 +31,14 @@ button.addEventListener("click", () => {
     output.innerHTML = `
              ${ageCal(name.value, age.value, gender.value)}
          `;
+         button.setAttribute('class','bg-danger text-white');
   }
 
   name.value = "";
   age.value = "";
 });
+
+button.addEventListener('blur', () => {
+       button.removeAttribute('class');
+       output.innerHTML = '';
+})
